c5bb8f006e1a04dfad08281e68b53d70b351000a
dlee <dlee@mozilla.com>
2015-04-17 17:03:45 +0800

    Update service worker

--- ServiceWorker/app.js
+++ ServiceWorker/app.js
@@ -1,9 +1,12 @@
 // register service worker
 
 var logger = document.getElementById("log");
+
+/*
 if ('serviceWorker' in navigator) {
   logger.innerHTML = "with service worker";
 } else {
   logger.innerHTML = "without service worker";
 }
+*/
 };
