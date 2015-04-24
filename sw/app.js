11ad646950a64b7abcca54c163cafda7499661e2
dlee <dlee@mozilla.com>
2015-04-24 11:44:38 +0800

    add error

--- sw/app.js
+++ sw/app.js
@@ -5,6 +5,8 @@ if ('serviceWorker' in navigator) {
 //  navigator.serviceWorker.register('https://dimil.github.io/sw/sw.js').then(function(reg) {
   navigator.serviceWorker.register('./sw.js').then(function(reg) {
     console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch((err) => {
+    console.log('Registration fail : ' + JSON.stringify(err));
   });
 } else {
   console.log("Without service worker");
