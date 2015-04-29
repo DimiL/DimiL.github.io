8764f17c9a5048981de4d34b247040a9f309a837
dlee <dlee@mozilla.com>
2015-04-29 11:54:59 +0800

    testing

--- sw/app.js
+++ sw/app.js
@@ -4,7 +4,7 @@ console.log("Dimi...run app.js");
 if ('serviceWorker' in navigator) {
   console.log("With service worker");
 //  navigator.serviceWorker.register('https://dimil.github.io/sw/sw.js').then(function(reg) {
-  navigator.serviceWorker.register('./sw.js').then(function(reg) {
+  navigator.serviceWorker.register('./monitor/sw.js').then(function(reg) {
     console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch((err) => {
     console.log('Registration fail : ' + JSON.stringify(err));
