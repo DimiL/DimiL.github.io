2c2101a5ae8f516967ab79d981548f3a24c85e55
dlee <dlee@mozilla.com>
2015-04-30 14:52:59 +0800

    complicate it

---
+++ sw/app2.js
@@ -0,0 +1,31 @@
+// register service worker
+console.log("Dimi...run app.js");
+
+/*
+if ('serviceWorker' in navigator) {
+  console.log("With service worker");
+//  navigator.serviceWorker.register('https://dimil.github.io/sw/monitor/sw.js').then(function(reg) {
+    navigator.serviceWorker.register('./monitor/sw.js').then(function(reg) {
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch((err) => {
+    console.log('Registration fail : ' + JSON.stringify(err));
+  });
+} else {
+  console.log("Without service worker");
+}
+*/
+
+if (!!window.SharedWorker) {
+  console.log("With shared workers >>");
+  var myWorker = new SharedWorker("/sw/worker/worker.js");
+  myWorker.port.start();
+  myWorker.port.postMessage([12,12]);
+  console.log('Message posted to worker');
+  console.log("With shared workers <<");
+
+  myWorker.port.onmessage = function(e) {
+    console.log('Message received from worker = ' + e.data);
+  }
+} else {
+  console.log("Without shared workers...");
+}
--- sw/worker/worker.js
+++ sw/worker/worker.js
@@ -1,9 +1,13 @@
-console.log("shared worker is running...");
+var tmp = 0;
+
+console.log("shared worker is running... " + tmp);
 
 onconnect = function(e) {
   console.log("shared worker on connect...");
   var port = e.ports[0];
   port.onmessage = function(e) {
+    console.log("shared worker before calculate " + tmp);
+    tmp = e.data[0] * e.data[1];
     var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
     console.log("shared worker calculate " + workerResult);
     port.postMessage(workerResult);
