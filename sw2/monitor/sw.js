0287b193d6a88c094107f3d10893aa6a013f5a19
DimiL <dlee@mozilla.com>
2015-05-28 18:31:22 +0800

    bug

---
+++ sw2/monitor/sw.js
@@ -0,0 +1,44 @@
+console.log("[SW]running");
+
+this.addEventListener('install', function(event) {
+  console.log("[SW]install");
+  var urlsToCache = [
+    '/imgs/f1.png',
+  ];
+
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      console.log("cache add");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  console.log("[SW]fetch v1 : " + JSON.stringify(event.request));
+  event.respondWith(new Response("Hello world!"));
+/*
+  var cachedResponse = caches.match(event.request).then(function(r) {
+    console.log("[SW]fetch ok, response = " + JSON.stringify(r));
+  }).catch(function() {
+    console.log("[SW]fetch not ok");
+  });
+*/
+/*
+  var response;
+  var cachedResponse = caches.match(event.request).catch(function() {
+    console.log("[SW]fetch response fail");
+//    return fetch(event.request);
+  }).then(function(r) {
+    console.log("[SW]fetch response ok?");
+//    console.log("[SW]fetch response : " + JSON.stringify(r));
+//    response = r;
+//    caches.open('v1').then(function(cache) {
+//      cache.put(event.request, response);
+//    });
+//    return response.clone();
+  }).catch(function() {
+//    return caches.match('/sw-test/gallery/myLittleVader.jpg');
+  });
+*/
+});
