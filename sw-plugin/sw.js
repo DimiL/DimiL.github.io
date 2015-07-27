93f361ae351cd1ebd712f531a2b2a71c2c3ce514
DimiL <dlee@mozilla.com>
2015-07-27 17:37:24 +0800

    sw plugin

---
+++ sw-plugin/sw.js
@@ -0,0 +1,39 @@
+this.addEventListener('install', function(event) {
+  console.log("[Dimi]install event v5");
+
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/sw-test/',
+        '/sw-test/index.html',
+        '/sw-test/style.css',
+        '/sw-test/image-list.js',
+        '/sw-test/star-wars-logo.jpg',
+        '/sw-test/gallery/',
+        '/sw-test/gallery/bountyHunters.jpg',
+        '/sw-test/gallery/myLittleVader.jpg',
+        '/sw-test/gallery/snowTroopers.jpg'
+      ]);
+    })
+  );
+});
+
+this.addEventListener('activate', function() {
+  console.log('[Dimi]activate event v1');
+});
+
+this.addEventListener('fetch', function(event) {
+  console.log("[Dimi]fetch event request: " + event.request.url);
+  var response;
+  var cachedResponse = caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('/sw-test/gallery/myLittleVader.jpg');
+  });
+});
