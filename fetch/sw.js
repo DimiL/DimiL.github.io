2f70f1a8a3aeb91fec7ae74249ecdda57e5ebddd
DimiL <dlee@mozilla.com>
2016-01-28 10:46:14 +0800

    add version

--- fetch/sw.js
+++ fetch/sw.js
@@ -1,12 +1,14 @@
 this.addEventListener('install', function(event) {
-  console.log("[Dimi][App]Install 1>>");
+  console.log("[Dimi][App]Install 5>>");
 });
 
 this.addEventListener('fetch', function(event) {
   console.log("[Dimi][App]Fetch 1>>");
 
 //  url = "https://dimil.github.io/sw-test/gallery/myLittleVader.jpg";
-  url = "https://c2.staticflickr.com/6/5809/23045784234_4bb7f9d076_k.jpg";
+//  url = "https://c2.staticflickr.com/6/5809/23045784234_4bb7f9d076_k.jpg";
+  url = "https://mdn.github.io/sw-test/gallery/myLittleVader.jpg";
+
   var req = new Request(url, { mode : 'no-cors',
                                method : 'GET' });
 
